af3c240ea200647a8fca536dd4e0cbd950527595
Victor <Valexandre@users.noreply.github.com>
2023-07-19 13:04:00 +0200

    Update multiscrap.R

--- multiscrap.R
+++ multiscrap.R
@@ -4,6 +4,7 @@ library(rvest)
 library(lubridate)
 library(calendar)
 library(magrittr)
+library(purrr)
 pages_a_scraper <-
   read.csv(
     "https://raw.githubusercontent.com/Valexandre/dates_publications/main/pagesascraper.csv"
